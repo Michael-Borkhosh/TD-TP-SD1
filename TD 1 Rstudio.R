8bc6adc097989497f78fac4b3791e3d27f6e74bc
Michael-Borkhosh <mborkhosh@gmail.com>
2026-01-26 15:13:03 +0100

    Add files via upload

---
+++ TD 1 Rstudio.R
@@ -0,0 +1,78 @@
+a = 10
+b = 5
+resultat = a * b
+print(resultat)
+
+A = 7.2
+B = 10.1
+resultat = A * B
+rm(a)
+rm(b, A, B, resultat)
+vecteur numerique = c(1,2,3,4,5)
+
+vecteur_numerique = c(1,2,3,4,5)
+class(3)
+
+sajid = c(TRUE,FALSE,TRUE,FALSE,FALSE,TRUE)
+class(sajid)
+
+vecteur_decimal = c(1.2, 6.7, 6.9)
+class(vecteur_decimal)
+vecteur_decimal[-3]
+
+mois = c("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre")
+class(mois)
+mois[c(1,2,3)]
+
+negatif = c(-1, -2, -3, -4, -5)
+class(negatif)
+negatif[c(5,1)]
+
+fruits = c("poire","raisin", "pasteque", "mangue")
+class(fruits)
+fruits[-c(1,2)]
+
+valmanque = c(1, 2, NA, 4, 5)
+class(valmanque)
+
+
+
+
+vecalea = runif(n = 5, min = 0, max = 1)
+mean(vecalea)
+median(vecalea)
+min(vecalea)
+max(vecalea)
+
+
+vecseq = runif(n = 10, min = -5, max = 5)
+mean(vecseq)
+median(vecseq)
+min(vecseq)
+max(vecseq)
+
+
+echantillon = runif(n = 100, min = 10, max = 20)
+mean(echantillon)
+median(echantillon)
+min(echantillon)
+max(echantillon)
+
+
+sequence2 = runif(n= 15, min = 50, max = 100)
+mean(sequence2)
+median(sequence2)
+min(sequence2)
+max(sequence2)
+
+
+
+
+
+
+
+
+
+
+
+
